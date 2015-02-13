7044fc2f45b745aad8bc4abd59ce60ffb215eff6
raclaass <ralf.claassens@asml.com>
2015-02-13 15:07:49 +0100

    Step 3: bare generated step definitions added

---
+++ features/step_definitions/file_viewer_steps.rb
@@ -0,0 +1,7 @@
+Given(/^I am in the root of my filesystem$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I expect to see my files$/) do
+  pending # express the regexp above with the code you wish you had
+end
